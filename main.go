62bb0080a4feb1f3f11f35e9993b00efa7a42ef2
tresor <tresorluz21@gmail.com>
2022-03-02 13:53:28 +0200

    Different types of Formatted strings

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	// Variable declaration
+	age := 33
+	ageTwo := "34"
+	name := "Tresor"
+
+	// This Print statement doesn't add a newline
+	fmt.Print("Hello, ")
+	fmt.Print("World! \n")
+
+	// This Print statement add a newline automatically
 	fmt.Println("Hello world")
+	// Inject variables in print statement
+	fmt.Println("My name is", name, "and I'm", age, "Years old")
+
+	// Formatted strings %_ format specifier
+	//%v for variable %q to get quotes around string %T for data type
+	fmt.Printf("My name is %v and I'm %v \n", name, age)
+	fmt.Printf("My name is %q and I'm %v \n", name, age)
+	fmt.Printf("My name is %T and I'm %T \n", name, age)
+	fmt.Printf("Your score is %f points \n", 255.55)
+	fmt.Printf("Your score is %0.1f \n", 255.55)
+
+	//sprintf save formatted strings
+	var str = fmt.Sprintf("My name is %v and I'm %v \n", name, ageTwo)
+	fmt.Println("The saved string is: ",str)
 }

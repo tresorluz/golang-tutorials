8b5504f5940ea1db8df27dc69f345dc0bbfcb1e8
tresor <tresorluz21@gmail.com>
2022-03-02 18:03:46 +0200

    Standard golang libraries

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	greetings := "Hello there friends!"
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+
+	fmt.Println(strings.Contains(greetings, "Hello"))
+	fmt.Println((strings.ReplaceAll(greetings, "Hello", "Hi")))
+	fmt.Println(strings.ToUpper(greetings))
+	fmt.Println(strings.Index(greetings, "ll"))
+	fmt.Println(strings.Split(greetings, " "))
+
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
+
+	names := []string{"Tresor", "Victoria", "Elyon", "Luzingamu"}
+	sort.Strings(names)
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "Victoria"))
 }

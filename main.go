8ea8900b7d7060a979c2b5f5d0d79c105b0bd9e8
tresor <tresorluz21@gmail.com>
2022-03-03 11:31:33 +0200

    functions

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+
+func sayGreeting(name string){
+	fmt.Printf("Good morning %v \n", name)
+}
+func sayBye(name string){
+	fmt.Printf("Goodbye %v \n", name)
+}
+func cycleNames(n []string, f func(string)){
+	for _, v := range n {
+		f(v)
+	}
+}
+
+func circleArea(r float64) float64{
+	return math.Pi * r * r
+}
 func main() {
-	fmt.Println("Hello world")
+	names := []string{"Tresor", "Victoria", "Elyon", "Luzingamu"}
+
+	sayGreeting("Tresor")
+	sayGreeting("Victoria")
+	sayBye("Elyon")
+	cycleNames(names, sayGreeting)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+
+	fmt.Println(a1, a2)
 }

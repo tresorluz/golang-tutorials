5cbb1d8ba9ac766fb3ed6539820a235b0ce6cd10
tresor <tresorluz21@gmail.com>
2022-03-03 21:47:58 +0200

    maps

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	//looping through a map
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "Tresor",
+		984759373: "Victoria",
+		845775485: "Elyon",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[267584967])
 }

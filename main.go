3d9c64c290dd1519b1f46afc1cbddab24c0e5a82
tresor <tresorluz21@gmail.com>
2022-03-04 12:49:58 +0200

    receiver function with pointer

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	mybill := newBill("Tresor's bill")
+
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 8.95)
+	mybill.addItem("toffee pudding", 4.95)
+	mybill.addItem("coffee", 3.25)
+
+	mybill.updateTip(10)
+	
+	fmt.Println(mybill.format())
 }
